903988c319af4213d7e3c036964ce4b8fe387d85
Simar Bhatia <119799025+SimarBhatiaSB7@users.noreply.github.com>
2026-07-28 22:28:10 +0530

    Time: 0 ms (100%), Space: 10.5 MB (76.89%) - LeetHub

---
+++ 0223-rectangle-area/0223-rectangle-area.cpp
@@ -0,0 +1,17 @@
+#include <algorithm>
+
+class Solution {
+public:
+    int computeArea(int ax1, int ay1, int ax2, int ay2, 
+                    int bx1, int by1, int bx2, int by2) {
+        
+        int area1 = (ax2 - ax1) * (ay2 - ay1);
+        int area2 = (bx2 - bx1) * (by2 - by1);
+
+        int overlap_x = std::max(0, std::min(ax2, bx2) - std::max(ax1, bx1));
+        int overlap_y = std::max(0, std::min(ay2, by2) - std::max(ay1, by1));
+        int overlap_area = overlap_x * overlap_y;
+
+        return area1 + area2 - overlap_area;
+    }
+};
