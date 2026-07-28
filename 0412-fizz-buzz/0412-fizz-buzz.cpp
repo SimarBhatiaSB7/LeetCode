4172631a72c1b51fb986b28c63dd34f3113d0808
Simar Bhatia <119799025+SimarBhatiaSB7@users.noreply.github.com>
2026-07-29 00:24:31 +0530

    Time: 0 ms (100%), Space: 11.8 MB (48.27%) - LeetHub

---
+++ 0412-fizz-buzz/0412-fizz-buzz.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    vector<string> fizzBuzz(int n) {
+        std::vector<string> answer;
+        for(int i = 0; i<=n; i++){
+            if(i%3 == 0 && i%5 == 0){
+                answer.push_back("FizzBuzz");
+            }
+            else if(i%3 == 0){
+                answer.push_back("Fizz");
+            }
+            else if(i%5 == 0){
+                answer.push_back("Buzz");
+            }
+            else{
+                answer.push_back(std::to_string(i));
+            }
+        }
+        answer.erase(answer.begin());
+        return answer;
+    }
+};
