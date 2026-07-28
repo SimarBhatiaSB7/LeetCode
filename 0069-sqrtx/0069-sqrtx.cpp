cb055a5decf0fb368cf375553c54034d19c89782
Simar Bhatia <119799025+SimarBhatiaSB7@users.noreply.github.com>
2026-07-28 23:04:11 +0530

    Time: 0 ms (100%), Space: 8.6 MB (14.93%) - LeetHub

--- 0069-sqrtx/0069-sqrtx.cpp
+++ 0069-sqrtx/0069-sqrtx.cpp
@@ -1,16 +1,25 @@
 class Solution {
 public:
     int mySqrt(int x) {
-        if (x < 2) return x;
-        int n = x / 2;      
-        for (long long i = 1; i <= n; i++) {
-            if (i * i == x) {
-                return i;
-            }
-            if (i * i > x) {
-                return i - 1;
-            }
+        if (x == 0 || x == 1)
+            return x;
+        
+        int start = 1;
+        int end = x;
+        int mid = -1;
+        
+        while (start <= end) {
+            mid = start + (end - start) / 2;
+            
+            long long square = static_cast<long long>(mid) * mid;
+
+            if (square > x)
+                end = mid - 1;
+            else if (square == x)
+                return mid;
+            else
+                start = mid + 1;
         }
-        return n;
+        return static_cast<int>(std::round(end));
     }
 };
