6a32d15a1d4d32112ca544affe04541859d1f86d
Simar Bhatia <119799025+SimarBhatiaSB7@users.noreply.github.com>
2026-07-29 22:40:07 +0530

    Time: 0 ms (100%), Space: 8.2 MB (16.77%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.cpp
+++ 0509-fibonacci-number/0509-fibonacci-number.cpp
@@ -1,9 +1,15 @@
 class Solution {
 public:
     int fib(int n) {
-        if(n == 0 || n == 1){
+        if (n == 0 || n == 1){
             return n;
         }
-        return fib(n - 1) + fib(n - 2);
+
+        std::vector<int> nums = {0,1};
+        for(int i = 2; i<=n; i++){
+            int f = nums[i - 1] + nums[i - 2];
+            nums.push_back(f);
+        }
+        return nums.back();
     }
 };
