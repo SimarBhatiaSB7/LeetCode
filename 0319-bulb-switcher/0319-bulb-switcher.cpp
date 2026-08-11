4356513bcfddd1c785617cb648dd80f0f8dfd7b7
Simar Bhatia <119799025+SimarBhatiaSB7@users.noreply.github.com>
2026-08-12 02:06:40 +0530

    Time: 0 ms (100%), Space: 7.8 MB (69.97%) - LeetHub

--- 0319-bulb-switcher/0319-bulb-switcher.cpp
+++ 0319-bulb-switcher/0319-bulb-switcher.cpp
@@ -1,6 +1,12 @@
 class Solution {
 public:
     int bulbSwitch(int n) {
-        return sqrt(n);
+        int sum = 1, i = 1, ans = 0;
+        while (sum <= n) {
+            ans++;
+            i += 2;
+            sum += i;
+        }
+        return ans;
     }
 };
