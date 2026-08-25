a11fee60974f2c7f7fe9510bc0709a389b7d3d12
Simar Bhatia <119799025+SimarBhatiaSB7@users.noreply.github.com>
2026-08-26 00:47:25 +0530

    Time: 0 ms (100%), Space: 24.1 MB (67.36%) - LeetHub

--- 3718-smallest-missing-multiple-of-k/3718-smallest-missing-multiple-of-k.cpp
+++ 3718-smallest-missing-multiple-of-k/3718-smallest-missing-multiple-of-k.cpp
@@ -1,6 +1,19 @@
 class Solution {
 public:
     int missingMultiple(vector<int>& nums, int k) {
-        
+        int n = nums.size();
+        int ans;
+        for(int i = k; i>n*k + k; i=i+k){
+            bool found = find(nums.begin(), nums.end(), i) != nums.end();
+            
+            if(found){
+                continue;
+            } 
+            else{
+                ans = i;
+                break;
+            }
+        }
+        return ans;
     }
 };
