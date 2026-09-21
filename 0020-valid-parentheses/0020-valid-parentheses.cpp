99f6950a9c1ad0305f66d4d9389d5f00d9002bad
Simar Bhatia <119799025+SimarBhatiaSB7@users.noreply.github.com>
2026-09-21 12:21:20 +0530

    Time: 0 ms (100%), Space: 8.9 MB (66.84%) - LeetHub

---
+++ 0020-valid-parentheses/0020-valid-parentheses.cpp
@@ -0,0 +1,26 @@
+class Solution {
+private:
+    bool isPair(char last, char cur) {
+        return (last == '(' && cur == ')') ||
+               (last == '{' && cur == '}') ||
+               (last == '[' && cur == ']');
+    }
+    
+public:
+    bool isValid(string s) {
+        stack <char> st;
+
+        for(char cur : s){
+            if(!st.empty()){
+                char last = st.top();
+                if(isPair(last,cur)){
+                    st.pop();
+                    continue;
+                }
+            }
+            st.push(cur);
+        }
+
+        return st.empty();
+    }
+};
